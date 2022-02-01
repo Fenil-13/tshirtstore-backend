035621c6cd52075bf5f11286c12dc28558bdd373
Fenil Moradiya <fjmoradiya@gmail.com>
2022-02-01 18:22:39 +0530

    Add Review

--- controller/productController.js
+++ controller/productController.js
@@ -72,6 +72,52 @@ exports.getOneProduct = BigPromise(async (req, res, next) => {
     })
 })
 
+exports.addReview = BigPromise(async (req, res, next) => {
+    const { rating, comment, productId } = req.body;
+
+    const review = {
+        user: req.user._id,
+        name: req.user.name,
+        rating: Number(rating),
+        comment
+    }
+    const product = await Product.findById(req.body.id)
+
+    if (!product) {
+        return next(new CustomError('No Product Found', 401))
+    }
+
+    const alreadyReview = product.reviews.find(
+        (rev) => rev.user.toString() === req.user._id.toString()
+    )
+
+    if (alreadyReview) {
+        product.reviews.forEach((review) => {
+            if (review.user.toString() === req.user._id.toString()) {
+                review.comment = comment;
+                review.rating = rating
+            }
+        });
+    } else {
+        product.reviews.push(review)
+        product.numberOfReviews = product.reviews.length
+    }
+
+    //adjust rating
+    product.ratings = product.reviews.reduce((acc, item) => item.rating + acc, 0)
+        / product.reviews.length;
+
+    //save
+    await product.save({
+        validateBeforeSave: false
+    })
+
+    res.status(200).json({
+        success: true,
+        product
+    })
+})
+
 
 //admin only code
 exports.adminGetAllProduct = BigPromise(async (req, res, next) => {
